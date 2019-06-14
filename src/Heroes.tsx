b35b7fe58572ed4c51a099812a4492e99b6261cd
Matthew Miller <matthew.miller@acadian.com>
2019-06-14 18:13:10 -0500

    Replaces mapDispatch with direct reference to actions.

--- src/Heroes.tsx
+++ src/Heroes.tsx
@@ -1,6 +1,5 @@
 import * as React from 'react';
 import { connect } from 'react-redux';
-import { Dispatch } from 'redux';
 import { State } from './index';
 import * as actions from './actions';
 import './Heroes.css';
@@ -8,17 +7,17 @@ import './Heroes.css';
 export interface Props {
   id: number;
   name: string;
-  onNameChange?: (name: string) => void;
+  nameChange?: (name: string) => void;
 }
 
-export function Heroes({ id, name, onNameChange=(() => null) }: Props) {
+export function Heroes({ id, name, nameChange=(() => null) }: Props) {
   return (
     <div className="hero">
       <h2>{name.toUpperCase()} Details</h2>
       <div><span>id: </span>{id}</div>
       <div>
         <label>name:
-          <input placeholder="name" value={name} onChange={(e) => onNameChange(e.target.value)}/>
+          <input placeholder="name" value={name} onChange={(e) => nameChange(e.target.value)}/>
         </label>
       </div>
     </div>
@@ -29,10 +28,4 @@ function mapState({ hero }: State) {
   return hero;
 }
 
-function mapDispatch(dispatch: Dispatch<actions.HeroAction>) {
-  return {
-    onNameChange: (name: string) => dispatch(actions.nameChange(name)),
-  };
-}
-
-export default connect(mapState, mapDispatch)(Heroes);
+export default connect(mapState, actions)(Heroes);
